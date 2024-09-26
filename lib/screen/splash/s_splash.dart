7f792e196b1011285b934d0844921cdebe224402
yonguilee <yongui@maegbug-ui-MacBookPro.local>
2024-09-26 13:37:01 +0900

    example of splash screen(basic)

---
+++ lib/screen/splash/s_splash.dart
@@ -0,0 +1,38 @@
+import 'package:after_layout/after_layout.dart';
+import 'package:fast_app_base/common/common.dart';
+import 'package:fast_app_base/common/dart/extension/num_duration_extension.dart';
+import 'package:fast_app_base/screen/main/s_main.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> with AfterLayoutMixin {
+  @override
+  FutureOr<void> afterFirstLayout(BuildContext context) {
+    Nav.clearAllAndPush(const MainScreen());
+  }
+
+  @override
+  void initState() {
+    delayTimer(() {
+      //.clearAllAndPush : history 모두 삭제된 후 이동
+      //.push
+    }, 1500.ms);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Image.asset(
+      "assets/image/splash/splash.png",
+      width: 192,
+      height: 192,
+    ));
+  }
+}
